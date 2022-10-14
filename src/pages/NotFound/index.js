8033e7067528e99aa8fe55effff72336814512e0
Dimas Setiaji <dimassetiaji30@gmail.com>
2022-10-14 17:22:01 +0700

    update sass

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -3,11 +3,11 @@ import './notfound.scss';
 
 const NotFound = () => {
   return (
-    <div className='container red'>
-      <div className='notfound'>
-        <h1 className='bordered'>NOT FOUND</h1>
+    <div className="container red">
+      <div className="notfound">
+        <h1 className="bordered">NOT FOUND</h1>
         <h1>NOT FOUND</h1>
-        <h1 className='bordered'>NOT FOUND</h1>
+        <h1 className="bordered">NOT FOUND</h1>
         <h1>NOT FOUND</h1>
         <h4>Halaman yang kamu cari tidak ditemukan...</h4>
       </div>
